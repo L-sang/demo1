13211b3097290fb918fe627a42b8b03c7ff6e466
L-sang <39798655+L-sang@users.noreply.github.com>
2018-06-18 19:34:09 +0800

    Add files via upload

---
+++ JDBC操作数据库/使用配置文件操作数据库/JDBC工具类/JDBCUtilsConfig.java
@@ -0,0 +1,45 @@
+package cn.itcast.jdbcutil;
+/*
+ * 编写JDBC的工具类,获取数据库的连接
+ * 采取读取配置文件的方式
+ * 读取配置文件,获取连接,执行一次,static{}
+ */
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class JDBCUtilsConfig {
+  
+	private static Connection con;
+	private static String driverClass;
+	private static String url;
+	private static String username;
+	private static String password;
+	
+	static {
+		try {
+			readConfig();
+			Class.forName(driverClass);
+			con = DriverManager.getConnection(url, username, password);
+		}catch(Exception ex) {
+			throw new RuntimeException("数据库连接失败");
+		}	
+	}
+	
+	private static void readConfig()throws Exception {
+		InputStream in = JDBCUtilsConfig.class.getClassLoader().getResourceAsStream("database.properties");
+		Properties pro = new Properties();
+		pro.load(in);
+		driverClass = pro.getProperty("driverClass");
+		url = pro.getProperty("url");
+		username = pro.getProperty("username");
+		password = pro.getProperty("password");
+	}
+	
+	public static Connection getConnection() {
+		return con;
+	}
+	
+}
